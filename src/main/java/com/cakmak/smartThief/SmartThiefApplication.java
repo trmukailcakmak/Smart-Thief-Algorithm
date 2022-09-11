a52077bf8ba790b6bafb7c2e338edb7586c80c4b
Mukail Cakmak <tr.mukailcakmak@gmail.com>
2022-09-11 21:09:51 +0300

    [Mukail Cakmak] initial commit.

---
+++ src/main/java/com/cakmak/smartThief/SmartThiefApplication.java
@@ -0,0 +1,13 @@
+package com.cakmak.smartThief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartThiefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartThiefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cakmak/smartThief/SmartThiefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cakmak.smartThief;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartThiefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
